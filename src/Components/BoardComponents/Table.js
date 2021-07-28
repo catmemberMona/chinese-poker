495031305eba81487858bebf2a5f2c279d4c516d
Mona Zheng <mona.a.zheng@gmail.com>
2021-07-28 16:48:46 -0400

    intalled material UI for Card Component added border-box for app sizing added cards to table section as hard coded

--- src/Components/BoardComponents/Table.js
+++ src/Components/BoardComponents/Table.js
@@ -1,13 +1,50 @@
 import React from 'react';
+import { Card } from '@material-ui/core';
+
+const CustomizeCard = () => {
+  return (
+    <Card style={ styles.card}>
+      <div style={styles.cardInner}>
+        <h1 style={{flex: 1}}>INT</h1>
+      </div>
+    </Card>
+  )
+}
+
+const Cards = () => {
+  return (
+    <div style={styles.cards}>
+      <CustomizeCard />
+      <CustomizeCard />
+      <CustomizeCard />
+      <CustomizeCard />
+      <CustomizeCard />
+    </div>
+  );
+}
 
 const Table = () => {
-  return <div style={styles.table}></div>;
+  return <div style={styles.table}>
+    <Cards />
+  </div>;
 };
 
 let styles = {
   table: {
     flex: 1,
-
   },
+  cards: {
+    flex: 1,
+    justifyContent: "space-evenly",
+    alignItems: "center",
+  },
+  card: {
+    height: 300,
+    width: 170,
+  },
+  cardInner: {
+    flex: 1,
+    margin: 5
+  }
 };
 export default Table;
