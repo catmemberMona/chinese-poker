5327831c308f19d8ee5038a6f0f9f78d53bfb207
Mona Zheng <mona.a.zheng@gmail.com>
2021-07-30 00:59:38 -0400

    install and set-up react-redux and redux created basic store and reducers outlines created gameReducer for game states

---
+++ src/store/reducers/gameReducer.js
@@ -0,0 +1,26 @@
+// Constants
+const SET_FIRST_GAME_TO_FALSE = 'SET_FIRST_GAME_TO_FALSE';
+const TOGGLE_IN_GAME = 'TOGGLE_IN_GAME';
+
+// Create actions
+const setFirstGameStateToFalse = () => ({ type: SET_FIRST_GAME_TO_FALSE} )
+const toggleInGameState = () => ({ type: TOGGLE_IN_GAME } )
+
+
+var initialState = {
+  isFirstGame: true,
+  isInPlay: false
+}
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case SET_FIRST_GAME_TO_FALSE:
+      return { ...state, isFirstGame: false };
+    case TOGGLE_IN_GAME:
+      return {...state, isInPlay: !state.isInPlay}
+    default:
+      return state;
+  }
+};
+
+export default reducer;
