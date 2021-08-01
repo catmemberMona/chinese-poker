c84620c711855e666632403a4200e1635fb4ec26
Mona Zheng <mona.a.zheng@gmail.com>
2021-07-31 23:05:44 -0400

    adjusted remove selected card action type so that it doesn't mutate the exisiting state added the action, get intial hand, ,to the player reducer

--- src/store/reducers/playerReducer.js
+++ src/store/reducers/playerReducer.js
@@ -1,6 +1,7 @@
 // Constants
 const ADD_SELECTED_CARD = 'ADD_SELECTED_CARD';
 const REMOVE_SELECTED_CARD = 'REMOVE_SELECTED_CARD';
+const GET_INITIAL_HAND = 'GET_INITIAL_HAND';
 
 // Create actions
 export const addSelectedCard = (card) => ({
@@ -13,18 +14,28 @@ export const removeCardsSelected = (card) => ({
   card,
 });
 
+export const givePlayerStack = (stack) => ({
+  type: GET_INITIAL_HAND,
+  cards
+})
+
 var initialState = {
+  hand: [], 
   cardsSelected: [],
 };
 
 const reducer = (state = initialState, action) => {
   switch (action.type) {
+    case GET_INITIAL_HAND:
+      return {...state, hand: [...action.stack]}
     case GET_SELECTED_CARD:
-      return { ...state, cardsSelected: [...state.cardsSelected, action.card]};
+      return { ...state, cardsSelected: [...state.cardsSelected, action.card] };
     case REMOVE_SELECTED_CARD:
-      const index = state.indexOf(action.card);
-      if (index) state.cardsSelected.splice(index, 1)
-      break
+      const index = state.cardsSelected.indexOf(action.card);
+      const selected = index
+        ? state.cardsSelected.splice(index, 1)
+        : state.cardsSelected;
+      return { ...state, cardsSelected: [...selected] };
     default:
       return state;
   }
