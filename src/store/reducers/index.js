e532b52065f1f996770255e6c994d9b236914528
Mona Zheng <mona.a.zheng@gmail.com>
2021-07-31 22:28:25 -0400

    add cards on table state

--- src/store/reducers/index.js
+++ src/store/reducers/index.js
@@ -1,8 +1,12 @@
 import game from './gameReducer';
 import { combineReducers } from 'redux';
 
+import table from './tableReducer'
+
 const rootReducer = combineReducers({
   game,
+  table
+
 });
 
 export default rootReducer;
