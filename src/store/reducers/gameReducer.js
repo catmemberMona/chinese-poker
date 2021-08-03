25b1ddd18a38144cec5ac172e3a325c9fdc6bdd8
Mona Zheng <mona.a.zheng@gmail.com>
2021-08-03 11:21:11 -0400

    added isPlayersTurn state to game

--- src/store/reducers/gameReducer.js
+++ src/store/reducers/gameReducer.js
@@ -1,15 +1,20 @@
 // Constants
 const SET_FIRST_GAME_TO_FALSE = 'SET_FIRST_GAME_TO_FALSE';
 const TOGGLE_IN_GAME = 'TOGGLE_IN_GAME';
+const ASSIGN_AS_PLAYERS_TURN = 'ASSIGN_AS_PLAYERS_TURN';
+const ASSIGN_AS_COMPUTERS_TURN = 'ASSIGN_AS_COMPUTERS_TURN';
 
 // Create actions
 export const setFirstGameStateToFalse = () => ({ type: SET_FIRST_GAME_TO_FALSE} )
-export const toggleInGameState = () => ({ type: TOGGLE_IN_GAME } )
+export const toggleInGameState = () => ({ type: TOGGLE_IN_GAME })
+export const setToPlayersTurn = () => ({ type: ASSIGN_AS_PLAYERS_TURN })
+export const setToComputersTurn = () => ({type: ASSIGN_AS_COMPUTERS_TURN})
 
 
 var initialState = {
   isFirstGame: true,
-  isInPlay: false
+  isInPlay: false,
+  isPlayersTurn: false,
 }
 
 const reducer = (state = initialState, action) => {
@@ -17,7 +22,11 @@ const reducer = (state = initialState, action) => {
     case SET_FIRST_GAME_TO_FALSE:
       return { ...state, isFirstGame: false };
     case TOGGLE_IN_GAME:
-      return {...state, isInPlay: !state.isInPlay}
+      return { ...state, isInPlay: !state.isInPlay }
+    case ASSIGN_AS_PLAYERS_TURN:
+      return { ...state, isPlayersTurn: true }
+    case ASSIGN_AS_COMPUTERS_TURN:
+      return {...state, isPlayersTurn: false}
     default:
       return state;
   }
