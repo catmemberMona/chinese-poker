fafb589493092526407e76a2e12f041db79ef1c3
Mona Zheng <mona.a.zheng@gmail.com>
2021-08-03 23:54:04 -0400

    increased width and changed positioning of modal
    Allow About modal to appear when site first renders

--- src/Components/BoardComponents/About.js
+++ src/Components/BoardComponents/About.js
@@ -2,25 +2,20 @@ import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Modal from '@material-ui/core/Modal';
 
-function rand() {
-  return Math.round(Math.random() * 20) - 10;
-}
 
 function getModalStyle() {
-  const top = 50 + rand();
-  const left = 50 + rand();
 
   return {
-    top: `${top}%`,
-    left: `${left}%`,
-    transform: `translate(-${top}%, -${left}%)`,
+    top: '11vh', 
+    left: '10vw',
   };
 }
 
 const useStyles = makeStyles((theme) => ({
   paper: {
     position: 'absolute',
-    width: 400,
+    width: '72vw',
+    minWidth: '30vw',
     backgroundColor: theme.palette.background.paper,
     border: '2px solid #000',
     boxShadow: theme.shadows[5],
@@ -32,7 +27,7 @@ export default function Info() {
   const classes = useStyles();
   // getModalStyle is not a pure function, we roll the style only on the first render
   const [modalStyle] = React.useState(getModalStyle);
-  const [open, setOpen] = React.useState(false);
+  const [open, setOpen] = React.useState(true);
 
   const handleOpen = () => {
     setOpen(true);
@@ -54,7 +49,7 @@ export default function Info() {
   return (
     <div>
       <button type='button' style={styles.buttons} onClick={handleOpen}>
-        Info and Rules
+        About the creator
       </button>
       <Modal
         open={open}
--- src/Components/BoardComponents/Info.js
+++ src/Components/BoardComponents/Info.js
@@ -2,25 +2,18 @@ import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Modal from '@material-ui/core/Modal';
 
-function rand() {
-  return Math.round(Math.random() * 20) - 10;
-}
-
 function getModalStyle() {
-  const top = 50 + rand();
-  const left = 50 + rand();
-
   return {
-    top: `${top}%`,
-    left: `${left}%`,
-    transform: `translate(-${top}%, -${left}%)`,
+    top: '11vh',
+    left: '10vw',
   };
 }
 
 const useStyles = makeStyles((theme) => ({
   paper: {
     position: 'absolute',
-    width: 400,
+    width: '72vw',
+    minWidth: '30vw',
     backgroundColor: theme.palette.background.paper,
     border: '2px solid #000',
     boxShadow: theme.shadows[5],
