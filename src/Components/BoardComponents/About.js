0b386d557a8ca5f9b25d6817dd608890b0954a5e
Mona Zheng <mona.a.zheng@gmail.com>
2021-08-05 14:36:34 -0400

    added: - landing section headings - about game content text - about developer image

--- src/Components/BoardComponents/About.js
+++ src/Components/BoardComponents/About.js
@@ -1,15 +1,8 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Modal from '@material-ui/core/Modal';
-
-
-function getModalStyle() {
-
-  return {
-    top: '11vh', 
-    left: '10vw',
-  };
-}
+import { Avatar } from '@material-ui/core';
+import mona from '../../Images/landingSection/mona.jpg'
 
 const useStyles = makeStyles((theme) => ({
   paper: {
@@ -21,12 +14,81 @@ const useStyles = makeStyles((theme) => ({
     boxShadow: theme.shadows[5],
     padding: theme.spacing(2, 4, 3),
   },
+  medium: {
+    width: theme.spacing(20),
+    height: theme.spacing(20),
+    border: '1px darkGrey solid',
+  },
 }));
+
+const ElementalPoker = () => {
+
+  const heading = (
+    <div style={styles.gameHeading}>
+      <h1>Elemental Poker</h1>
+      <div>Images</div>
+    </div>
+  );
+
+  const gameContent = (
+    <div style={styles.gameText}>
+      <div>
+        Elemental Poker is a single player game where the user competes with the
+        computer. Win by playing all the cards in your hand.
+      </div>
+      <div>
+        It is based on a traditional chinese card game. Traditionally chinese
+        poker is usually played with 2-4 players with a deck of 52 cards.
+      </div>
+
+      <div style={styles.aboutMintBean}>
+        <h3 style={{ border: '1px solid black' }}>Mintbean</h3>
+        <p style={{ border: '1px solid black' }}>
+          Elemental Poker was created during a weekly hackathon hosted by
+          MintBean. Mintbean provides a supportive community for software
+          developers. Learn more about this{' '}
+          <span>
+            <a href='https://mintbean.io/meets/7e2331fb-1e0d-4b31-86b9-a46acad877af'>
+              event!
+            </a>
+          </span>
+        </p>
+      </div>
+    </div>
+  );
+
+  return (
+    <div style={styles.aboutGame}>
+      {heading}
+
+      <div style={styles.gameContent}>
+        {gameContent}
+        <div style={styles.gameDemo}>Demo Video</div>
+      </div>
+    </div>
+  );
+}
+
+const Developer = (props) => {
+  const classes = props.classes
+  return (
+    <div style={styles.aboutDeveloper}>
+      <h1>Mona Zheng</h1>
+      <div style={styles.image}>
+        <Avatar
+          alt='Mona'
+          variant='rounded'
+          src={mona}
+          className={classes.medium}
+        />
+        <div style={styles.devText}>JKFJDKSFJKFJDKSFJKDLFJKSFJFDKFJDSKFJKDSFJDSKFJKDSFJKDSFJ</div>
+      </div>
+    </div>
+  );
+}
 
 export default function Info() {
   const classes = useStyles();
-  // getModalStyle is not a pure function, we roll the style only on the first render
-  const [modalStyle] = React.useState(getModalStyle);
   const [open, setOpen] = React.useState(true);
 
   const handleOpen = () => {
@@ -38,11 +100,10 @@ export default function Info() {
   };
 
   const body = (
-    <div style={modalStyle} className={classes.paper}>
-      <h2 id='simple-modal-title'>Text in a modal</h2>
-      <p id='simple-modal-description'>
-        Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
-      </p>
+    <div style={styles.container} className={classes.paper}>
+      <ElementalPoker />
+        <Developer classes={classes} />
+
     </div>
   );
 
@@ -70,6 +131,42 @@ let styles = {
     height: 50,
     borderRadius: 8,
     margin: 5,
-    backgroundColor: 'tan'
+    backgroundColor: 'tan',
+  },
+  container: {
+    top: '11vh',
+    left: '10vw',
+    flexDirection: 'column',
+  },
+  aboutDeveloper: {
+    flex: 1,
+    flexDirection: 'column',
+    marginLeft: '2vw',
+    // marginTop: 0
+  },
+  image: {},
+  devText: {
+    padding: '1vw',
+    marginLeft: '4vw',
+  },
+  aboutGame: {
+    flex: 1,
+    flexDirection: 'column',
+    marginLeft: '2vw',
+  },
+  gameContent: {},
+  gameText: {
+    flex: 3,
+    flexDirection: 'column',
+  },
+  gameDemo: {
+    flex: 2,
+  },
+  gameHeading: {
+    justifyContent: 'space-between',
+  },
+  aboutMintBean: {
+    flexDirection: 'column',
+    border: '1px solid black',
   },
 };
